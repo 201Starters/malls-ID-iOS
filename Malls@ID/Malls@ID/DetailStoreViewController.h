64dfb146db8cd5e7d9c4fa2aedd13108befa97b1
rezahadafi <rezahadafi@gmail.com>
2015-01-15 16:23:52 +0700

    nambahin detail store

---
+++ Malls@ID/Malls@ID/DetailStoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailStoreViewController.h
+//  Malls@ID
+//
+//  Created by reza hadafi on 1/15/15.
+//  Copyright (c) 2015 Farandi Kusumo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailStoreViewController : UITableViewController
+
+@end
